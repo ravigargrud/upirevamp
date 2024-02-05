943400d71696d8197055c04d32f591dd0ca1a9a6
Ravi <ravigargrud@gmail.com>
2024-02-05 17:06:35 +0530

    HomePage Half Done

--- upirevamp/src/App.jsx
+++ upirevamp/src/App.jsx
@@ -1,33 +1,64 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import notificationLogo from './assets/notifications.png'
 import './App.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <img src={reactLogo} className="react-logo" alt="react logo" />
+        <div id="headtxt">Howdy, <br></br>
+        <h3> Pearl</h3>
+        </div>
+        <div className="searchbar">
+      <input type="text" placeholder="Search..." />
+      <i className="fa fa-search"></i>
+    </div>
+    <img className="notifimg" src={notificationLogo} alt='notification'></img>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+
+    <div className='mainbody'>
+      <div className='listicons'>
+        <img src={viteLogo} className="vite-logo" alt="vite logo" />
+        <img src={viteLogo} className="vite-logo" alt="vite logo" />
+        <img src={viteLogo} className="vite-logo" alt="vite logo" />
+        <img src={viteLogo} className="vite-logo" alt="vite logo" />
+        <img src={viteLogo} className="vite-logo" alt="vite logo" />
+        <img src={viteLogo} className="vite-logo" alt="vite logo" />
+        <img src={viteLogo} className="vite-logo" alt="vite logo" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className='balancewindow'>
+        <div className='bwhead'>
+          <button>Delhi, IN</button>
+          11:45 PM
+        </div>
+        <div className='accbal'> Account Balance 
+          <img src={viteLogo} alt=''></img>
+        </div>
+        $100
+        <div className='balancebtns'>
+          <div className='tranbtn'>
+            Add Funds </div>
+          <div className='tranbtn'>Withdraw</div>
+        </div>
+        <div className='balancebtns'>
+          <div className='tranbtn'>Pay UPI ID</div>
+          <div className='tranbtn'>Pay Contact</div>
+        </div>
+        <div className='balancebtns'>
+        <div className='tranbtn'>Pay via NFC</div></div>
+
+      </div>
+    </div>
     </>
   )
 }
